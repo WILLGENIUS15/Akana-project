9c8b16f1fac5420ca3e7ae0816d7e3e8e01136a5
fidel omondi <willgenius@gmail.com>
2019-07-15 09:47:30 +0300

    adding functions

--- js/index.js
+++ js/index.js
@@ -2,3 +2,9 @@ function myFunction() {
   function day (cc,yy,mm,dd){
 return ((((cc/4)-2*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
 }
+var cc = parseInt(prompt("Enter the century"));
+var yy = parseInt(prompt("Enter the year"));
+var mm = parseInt(prompt("Enter the month"));
+var dd = parseInt(prompt("Enter the date of birth"));
+var gender= prompt("your gender")
+var result=day(cc,yy,mm,dd).toFixed();
